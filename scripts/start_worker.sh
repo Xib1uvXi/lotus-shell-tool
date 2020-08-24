fa2fbe2896b8a50f8fb8c254d1103c100f89134b
wangxi <xib1102@icloud.com>
2020-08-25 01:11:47 +0800

    finish

--- scripts/start_worker.sh
+++ scripts/start_worker.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-## $2 是日志
-echo "$1"
-nohup lotus-worker --worker-repo=$2 run --listen=$3 --addpiece=$4 --precommit1=$5 --precommit2=$6 --commit=$7 >>$8 2>&1 &
+mkdir -p $7
+export PATH=$PATH:~/tools/filecoin/calibration
+nohup lotus-worker --worker-repo=$1 run --listen=$2 --addpiece=$3 --precommit1=$4 --precommit2=$5 --commit=$6 >>$7 2>&1 &
